4147049dc45a6fb907a77ccf53af1da859df48da
Francois Dumas <dumaslattaque.francois@gmail.com>
2022-01-31 20:56:34 +0100

    Access to individual repo

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,18 @@
 class PagesController < ApplicationController
+  before_action :set_client, only: %i[repositories repository]
+
   def repositories
-    @client = Octokit::Client.new(access_token: "#{ENV["GITHUB_PERSONAL_TOKEN"]}")
-    @client.repos
     @my_repos = @client.repos({}, query: {type: 'owner', sort: 'asc'})
     @private_repos = @client.repos({}, query: {type: 'private', sort: 'asc'})
+  end
+
+  def repository
+    @repo = @client.repo(params[:repo_full_name])
+  end
+
+  private
+
+  def set_client
+    @client = Octokit::Client.new(access_token: "#{ENV["GITHUB_PERSONAL_TOKEN"]}")
   end
 end
