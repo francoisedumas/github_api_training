0b1be13f88686f83c7e2c51f5ef9d6070011a1d8
Francois Dumas <dumaslattaque.francois@gmail.com>
2022-02-01 18:18:58 +0100

    Add remove collaborator action

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
-  before_action :set_client, only: %i[repositories repository]
+  before_action :set_client, only: %i[repositories repository remove]
+  before_action :set_repo, only: %i[repository remove]
 
   def repositories
     @my_repos = @client.repos({}, query: {type: 'owner', sort: 'asc'})
@@ -14,13 +15,26 @@ class PagesController < ApplicationController
   def remove
     repo_full_name = params[:repo_full_name]
     collab_login = params[:collab_login]
-    flash[:notice] = "#{collab_login} has been removed successfully!"
-    redirect_to repository_path(repo_full_name: repo_full_name)
+
+    if @repo[:owner][:login] == collab_login
+      flash[:alert] = "Action can't be performed, as #{collab_login} is the owner of the repo"
+      redirect_to repository_path(repo_full_name: repo_full_name)
+    elsif @client.remove_collaborator(repo_full_name, collab_login)
+      flash[:notice] = "#{collab_login} has been removed successfully!"
+      redirect_to repository_path(repo_full_name: repo_full_name)
+    else
+      flash[:alert] = "#{collab_login} has not been removed!"
+      redirect_to repository_path(repo_full_name: repo_full_name)
+    end
   end
 
   private
 
   def set_client
     @client = Octokit::Client.new(access_token: "#{ENV["GITHUB_PERSONAL_TOKEN"]}")
+  end
+
+  def set_repo
+    @repo = @client.repo(params[:repo_full_name])
   end
 end
